1344a44dcb676db596dc277f0ded21c2bb1738a7
Matthew Allison <matthewallisonxyz@gmail.com>
2024-04-23 21:04:25 +0200

    cleaned up file of old code and added support for date offsets

--- times.py
+++ times.py
@@ -5,69 +5,30 @@ from astral.sun import sun
 from datetime import datetime, timedelta
 from pytz import timezone
 from timezonefinder import TimezoneFinder
-from num2words import num2words
 from convertdate import hebrew
 
 import hebrew_converter
 
 
 class Times:
-    joburg_data = {}
-    capetown_data = {}
-
     cities = cities.Cities()
     cities.cities()
     tf = TimezoneFinder()
 
-    # Old
-    def get_data(self):
-        url = "https://api.sunrise-sunset.org/json"
-        params = {
-            "lat": cities.CapeTown.latitude,
-            "lng": cities.CapeTown.longitude,
-            "date": "today",
-            "formatted": 0,
-            "tzid": "Africa/Johannesburg"
-        }
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-
-            self.capetown_data = data["results"]
-
-        params = {
-            "lat": cities.Johannesburg.latitude,
-            "lng": cities.Johannesburg.longitude,
-            "date": "today",
-            "formatted": 0,
-            "tzid": "Africa/Johannesburg"
-        }
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-
-            self.joburg_data = data["results"]
-
-    def dawn(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-        current_date = datetime.now(timezone('UTC')).date()
+    def dawn(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         dawn = sun(location.observer, date=current_date, dawn_dusk_depression=16.9)
         dawn_time_utc = dawn['dawn']
 
-        location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
-        adjusted_dawn_time = dawn_time_utc.astimezone(location_timezone)
+        city_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
+        dawn_time_city = dawn_time_utc.astimezone(city_timezone)
 
-        dawn_time = adjusted_dawn_time.strftime("%H:%M")
+        dawn_time = dawn_time_city.strftime("%H:%M")
         return dawn_time
 
-    def earliest_tallit_tefillin(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-        current_date = datetime.now(timezone('UTC')).date()
+    def earliest_tallit_tefillin(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         tallit_tefillin = sun(location.observer, date=current_date, dawn_dusk_depression=10.2)
         tallit_tefillin_utc = tallit_tefillin['dawn']
@@ -78,11 +39,8 @@ class Times:
         tallit_tefillin_time = adjust_tallit_tefillin.strftime("%H:%M")
         return tallit_tefillin_time
 
-    def sunrise(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-        current_date = datetime.now(timezone('UTC')).date()
+    def sunrise(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         sun_times = sun(location.observer, date=current_date)
         sunrise_utc = sun_times['sunrise']
@@ -93,11 +51,8 @@ class Times:
         sunrise_time = adjusted_sunrise.strftime("%H:%M")
         return sunrise_time
 
-    def latest_shema(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-        current_date = datetime.now(timezone('UTC')).date()
+    def latest_shema(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         hanetz_amiti = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)['dawn']
         location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
@@ -113,12 +68,8 @@ class Times:
 
         return latest_shema
 
-    def latest_shacharit(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-
-        current_date = datetime.now(timezone('UTC')).date()
+    def latest_shacharit(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
         hanetz_amiti = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)['dawn']
@@ -133,11 +84,8 @@ class Times:
 
         return latest_shacharit
 
-    def hanetz_amiti(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-        current_date = datetime.now(timezone('UTC')).date()
+    def hanetz_amiti(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         dawn = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)
 
@@ -149,11 +97,8 @@ class Times:
         hanetz_amiti_time = adjusted_dawn_time.strftime("%H:%M")
         return hanetz_amiti_time
 
-    def shkiah_amitis(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-        current_date = datetime.now(timezone('UTC')).date()
+    def shkiah_amitis(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         dusk = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)
 
@@ -166,12 +111,8 @@ class Times:
         shkiah_amitis_time = dawn_time_utc_plus_2.strftime("%H:%M")
         return shkiah_amitis_time
 
-    def midday(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-
-        current_date = datetime.now(timezone('UTC')).date()
+    def midday(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         start_time = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)['dawn']
         end_time = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)['dusk']
@@ -187,12 +128,8 @@ class Times:
         midday_time = midpoint_time_utc_plus_2.strftime("%H:%M")
         return midday_time
 
-    def earliest_mincha(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-
-        current_date = datetime.now(timezone('UTC')).date()
+    def earliest_mincha(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         hanetz_amiti = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)['dawn']
         location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
@@ -209,12 +146,8 @@ class Times:
 
         return earliest_mincha
 
-    def mincha_ketana(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-
-        current_date = datetime.now(timezone('UTC')).date()
+    def mincha_ketana(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         shkiah_amitis = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)['dusk']
         location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
@@ -231,12 +164,8 @@ class Times:
 
         return mincha_ketana_time
 
-    def plag_hamincha(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-
-        current_date = datetime.now(timezone('UTC')).date()
+    def plag_hamincha(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         shkiah_amitis = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)['dusk']
         location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
@@ -253,12 +182,8 @@ class Times:
 
         return plag_hamincha_time
 
-    def sunset(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-
-        current_date = datetime.now(timezone('UTC')).date()
+    def sunset(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         sun_times = sun(location.observer, date=current_date)
 
@@ -271,12 +196,8 @@ class Times:
         sunset_time = sunset_utc_plus_2.strftime("%H:%M")
         return sunset_time
 
-    def nightfall(self, city):
-        latitude, longitude = self.cities.get_coordinates(city)
-
-        location = LocationInfo(longitude=longitude, latitude=latitude)
-
-        current_date = datetime.now(timezone('UTC')).date()
+    def nightfall(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
 
         nightfall = sun(location.observer, date=current_date, dawn_dusk_depression=6)
 
@@ -289,14 +210,17 @@ class Times:
         nightfall_time = nightfall_time_utc_plus_2.strftime("%H:%M")
         return nightfall_time
 
-    def midnight(self, city):
+    def common_values(self, city, offset=0):
         latitude, longitude = self.cities.get_coordinates(city)
-
         location = LocationInfo(longitude=longitude, latitude=latitude)
-
         current_date = datetime.now(timezone('UTC')).date()
-        tomorrow_date = current_date + timedelta(days=1)
+        current_date = current_date + timedelta(days=offset)
+        return current_date, latitude, location, longitude
 
+    def midnight(self, city, offset=0):
+        current_date, latitude, location, longitude = self.common_values(city, offset)
+
+        tomorrow_date = current_date + timedelta(days=1)
         start_time = sun(location.observer, date=current_date, dawn_dusk_depression=1.583)['dusk']
         end_time = sun(location.observer, date=tomorrow_date, dawn_dusk_depression=1.583)['dawn']
 
@@ -311,10 +235,10 @@ class Times:
         midnight_time = midpoint_time_utc_plus_2.strftime("%H:%M")
         return midnight_time
 
-    def shaah_zmanit(self, city):
+    def shaah_zmanit(self, city, offset=0):
 
-        time1 = self.hanetz_amiti(city)
-        time2 = self.shkiah_amitis(city)
+        time1 = self.hanetz_amiti(city, offset)
+        time2 = self.shkiah_amitis(city, offset)
 
         time1_obj = datetime.strptime(time1, "%H:%M")
         time2_obj = datetime.strptime(time2, "%H:%M")
@@ -330,23 +254,24 @@ class Times:
 
         return result
 
-    def get_current_hebrew_date(self, city):
+    def get_current_hebrew_date(self, city, offset=0):
         latitude, longitude = self.cities.get_coordinates(city)
         location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
         current_date = datetime.now(location_timezone).date()
+        current_date = current_date + timedelta(days=offset)
+
         hebrew_date = hebrew.from_gregorian(current_date.year, current_date.month, current_date.day)
         hebrew_date = str(hebrew_date)
         hebrew_date = hebrew_date.replace('(', ' ').replace(')', ' ').replace(',', '')
         return hebrew_date
 
-    def get_current_hebrew_date_words(self, city):
-        number_date = self.get_current_hebrew_date(city)
+    def get_current_hebrew_date_words(self, city, offset=0):
+        number_date = self.get_current_hebrew_date(city, offset)
         number_date = number_date.strip()
         number_date = number_date.split(' ')
         print(number_date)
         word_date = hebrew_converter.convert_to_words(number_date[0], number_date[1], number_date[2])
         return word_date
-
 
     def get_hebrew_date_30_days_ago(self, city):
         latitude, longitude = self.cities.get_coordinates(city)
@@ -358,26 +283,29 @@ class Times:
         hebrew_date = hebrew_date.replace('(', ' ').replace(')', ' ').replace(',', '')
         return hebrew_date
 
-
-    def get_current_english_date(self, city):
+    def get_current_english_date(self, city, offset=0):
         latitude, longitude = self.cities.get_coordinates(city)
         location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
         english_date = datetime.now(location_timezone).date()
+        english_date = english_date + timedelta(days=offset)
         english_date = str(english_date)
         english_date = english_date.replace('-', ' ')
         return english_date
 
-    def get_current_english_date_words(self, city):
+    def get_current_english_date_words(self, city, offset=0):
         latitude, longitude = self.cities.get_coordinates(city)
         location_timezone = timezone(self.tf.timezone_at(lng=longitude, lat=latitude))
         english_date = datetime.now(location_timezone).date()
+        english_date = english_date + timedelta(days=offset)
+
         english_date = english_date.strftime("%d %B %Y")
         return english_date
 
-    def is_friday(self, city):
+    def is_friday(self, city, offset=0):
         latitude, longitude = self.cities.get_coordinates(city)
         location = LocationInfo(longitude=longitude, latitude=latitude)
         current_date = datetime.now(timezone('UTC')).date()
+        current_date = current_date + timedelta(days=offset)
         day = sun(location.observer, date=current_date)['sunrise']
         day = day.strftime("%A")
         if day == "Friday":
@@ -385,42 +313,9 @@ class Times:
         else:
             return False
 
-    def candle_lighting(self, city):
-        sunset_time = self.sunset(city)
+    def candle_lighting(self, city, offset=0):
+        sunset_time = self.sunset(city, offset)
         sunset_time_obj = datetime.strptime(sunset_time, "%H:%M")
         sunset_time_obj = sunset_time_obj - timedelta(minutes=18)
         candle_lighting_time = sunset_time_obj.strftime("%H:%M")
         return candle_lighting_time
-
-
-    # Old
-    def debug(self):
-        times = Times()
-        print(times.sunrise(cities.CapeTown))
-        print(times.sunrise(cities.Johannesburg))
-        print(times.sunset(cities.CapeTown))
-        print(times.sunset(cities.Johannesburg))
-        print(times.dawn(cities.CapeTown))
-        print(times.dawn(cities.Johannesburg))
-        print(times.earliest_tallit_tefillin(cities.CapeTown))
-        print(times.earliest_tallit_tefillin(cities.Johannesburg))
-        print(times.shaah_zmanit(cities.CapeTown))
-        print(times.midday(cities.CapeTown))
-        print(times.latest_shema(cities.CapeTown))
-        print(times.latest_shema(cities.Johannesburg))
-        print(times.latest_shacharit(cities.CapeTown))
-        print(times.latest_shacharit(cities.Johannesburg))
-        print(times.earliest_mincha(cities.CapeTown))
-        print(times.earliest_mincha(cities.Johannesburg))
-        print(times.mincha_ketana(cities.CapeTown))
-        print(times.mincha_ketana(cities.Johannesburg))
-        print(times.plag_hamincha(cities.CapeTown))
-        print(times.plag_hamincha(cities.Johannesburg))
-        print(times.nightfall(cities.CapeTown))
-        print(times.nightfall(cities.Johannesburg))
-        print(times.midnight(cities.CapeTown))
-        print(times.midnight(cities.Johannesburg))
-
-
-
-
--- web.py
+++ web.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import csv
 import times
 import cities
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24).hex()
 Times = times.Times()
 
 
@@ -34,32 +36,48 @@ def home():
 def search():
     query = request.args.get('query')
     query = query.replace('+', ' ')
-    return index(query)
+    date_offset = int(request.args.get('date_offset', 0))
+    session['date_offset'] = date_offset
+    return index(query, date_offset)
 
 
-def index(selected_city="Cape Town"):
+@app.route('/update_offset', methods=['POST'])
+def update_offset():
+    city = request.form.get('city')
+    action = request.form.get('action')
+    current_offset = int(request.form.get('current_offset'))
+
+    if action == 'increment':
+        new_offset = current_offset + 1
+    else:
+        new_offset = current_offset - 1
+
+    return redirect(url_for('search', query=city, date_offset=new_offset))
+
+
+def index(selected_city="Cape Town", date_offset=0):
     # selected_city = request.args.get('selectedCity')
     if selected_city:
         city = selected_city
         print("selected City: ", city)
         # Call the relevant functions based on the selected city
-        dawn = Times.dawn(city)
-        earliest_tallit = Times.earliest_tallit_tefillin(city)
-        sunrise = Times.sunrise(city)
-        latest_shema = Times.latest_shema(city)
-        latest_shacharit = Times.latest_shacharit(city)
-        midday = Times.midday(city)
-        earliest_mincha = Times.earliest_mincha(city)
-        mincha_ketana = Times.mincha_ketana(city)
-        plag_hamincha = Times.plag_hamincha(city)
-        sunset = Times.sunset(city)
-        nightfall = Times.nightfall(city)
-        midnight = Times.midnight(city)
-        shaah_zmanit = round(Times.shaah_zmanit(city) * 60, 2)
-        current_date_hebrew = Times.get_current_hebrew_date_words(city)
-        current_date_english = Times.get_current_english_date_words(city)
-        is_friday = Times.is_friday(city)
-        candle_lighting = Times.candle_lighting(city)
+        dawn = Times.dawn(city, date_offset)
+        earliest_tallit = Times.earliest_tallit_tefillin(city, date_offset)
+        sunrise = Times.sunrise(city, date_offset)
+        latest_shema = Times.latest_shema(city, date_offset)
+        latest_shacharit = Times.latest_shacharit(city, date_offset)
+        midday = Times.midday(city, date_offset)
+        earliest_mincha = Times.earliest_mincha(city, date_offset)
+        mincha_ketana = Times.mincha_ketana(city, date_offset)
+        plag_hamincha = Times.plag_hamincha(city, date_offset)
+        sunset = Times.sunset(city, date_offset)
+        nightfall = Times.nightfall(city, date_offset)
+        midnight = Times.midnight(city, date_offset)
+        shaah_zmanit = round(Times.shaah_zmanit(city, date_offset) * 60, 2)
+        current_date_hebrew = Times.get_current_hebrew_date_words(city, date_offset)
+        current_date_english = Times.get_current_english_date_words(city, date_offset)
+        is_friday = Times.is_friday(city, date_offset)
+        candle_lighting = Times.candle_lighting(city, date_offset)
     else:
         # Cape Town is the default city
         dawn = Times.dawn("Cape Town")
@@ -83,11 +101,13 @@ def index(selected_city="Cape Town"):
 
     cities_list = read_cities_from_csv('cities.csv')
 
-    return render_template('times.html', current_date_english=current_date_english, current_date_hebrew=current_date_hebrew, dawn=dawn, earliest_tallit=earliest_tallit,
+    return render_template('times.html', current_date_english=current_date_english,
+                           current_date_hebrew=current_date_hebrew, dawn=dawn, earliest_tallit=earliest_tallit,
                            sunrise=sunrise, latest_shema=latest_shema, latest_shacharit=latest_shacharit,
                            midday=midday, earliest_mincha=earliest_mincha, mincha_ketana=mincha_ketana,
                            plag_hamincha=plag_hamincha, sunset=sunset, nightfall=nightfall, midnight=midnight,
-                           shaah_zmanit=shaah_zmanit, city=city, cities=cities_list, is_friday=is_friday, candle_lighting=candle_lighting)
+                           shaah_zmanit=shaah_zmanit, city=city, cities=cities_list, is_friday=is_friday,
+                           candle_lighting=candle_lighting)
 
 
 if __name__ == '__main__':
