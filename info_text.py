6f953aa1c7357a4b07aba1394f42277cef6ecfe1
Matthew Allison <66563291+Sir-Goose@users.noreply.github.com>
2024-09-26 17:21:03 +0200

    time calculation explanations

---
+++ info_text.py
@@ -0,0 +1,89 @@
+def dawn() -> str:
+    explanation = """Alot hashachar or dawn refers to the time at which the first rays of light are
+    visible in the morning. This is calculated to be when the centre point of the sun's disk is 16.9 degrees
+    below the horizon.
+    """
+    return explanation
+
+def earliest_tallit():
+    explanation = """Misheyakir occurs jast after dawn. It is traditionally defined as
+    the time at which one can recognize another person four cubits away. This is the earliest time to
+    wear tzitzit and tefilin and to recite the Shema.
+
+    This is calculated to be when the centre point of the sun's disk is 10.2 degrees below the horizon."""
+    return explanation
+
+def sunrise():
+    explanation = """Sunrise is the moment when the top of sun's disk becomes
+    visible at sea level. One should begin the morning Amidah no earlier than this time.
+    Most daytime mitzvot should be performed after this time."""
+    return explanation
+
+def latest_shema():
+    explanation = """The latest time to recite the morning Shema.
+    This is the end of the first quater of a halachic day.
+    This means three 'proportional' hours or shaos zmaniyos have passed.
+
+    This is then simply calculated by adding three proportional hours to the
+    halachic sunrise."""
+    return explanation
+
+def latest_shacharit():
+    explanation = """The latest time to say the morning Amidah.
+    It is the end of the first third of a halachic day. This is
+    simply calculated by adding four proportional hours to the halachic
+    sunrise."""
+    return explanation
+
+def midday():
+    explanation = """Chatzos is the halachic noon and the midpoint of the
+    halachic day. This is calculated as the midpoint between the halachic sunrise
+    and sunset."""
+    return explanation
+
+def earliest_mincha():
+    explanation = """This is the earliest time to recite Mincha.
+    It is defined as half a halachic hour after midday and therefore calculated
+    as 6.5 proportional hours after halachic sunrise.
+    """
+    return explanation
+
+def mincha_ketanah():
+    explanation = """Mincha ketanah litterally translated as the smaller Mincha
+    is considered the ideal time for praying Mincha.
+    It is calculated as 2.5 proportional hours before the halachic sunset.
+    """
+    return explanation
+
+def plag_hamincha():
+    explanation = """The midpoint between Mincha ketanah and sunset as well
+    as the earliest time one may light Shabbat candles.
+    It is calculated as 1.25 proportional hours before the halachic sunset."""
+    return explanation
+
+def sunset():
+    explanation = """Sunrise is the moment when the top of the sun's disk
+    falls below the horizon. A new day in the Hebrew calendar begins at this
+    time. All daytime mitzvot should be completed before sunset."""
+    return explanation
+
+def nightfall():
+    explanation = """Nightfall is generally considered to being when three
+    stars become visible in the night sky. This marks the time from which all
+    nighttime mitzvot may be performed such as reciting the evening Shema.
+    It is calculated as when the centre of the sun's disk is 8.5 degrees below
+    the horizon.
+    """
+    return explanation
+
+def midnight():
+    explanation = """One should try to complete all nightime mitzvot
+    before this time such as the nightly Shema and the counting of the Omer.
+    Chatzot or halachic midnight is calculated as the midpoint between
+    halachic sunrise and halachic sunset."""
+    return explanation
+
+def shaah_zmanit():
+    explanation = """A halachic or proportional hour. Shaos zmaniyos are
+    each 1/12th of the halachic day."""
+    return explanation
