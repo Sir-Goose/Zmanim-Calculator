87eb683bf1caf3d718f93ff99d0e46a8c1d15670
Matthew Allison <66563291+Sir-Goose@users.noreply.github.com>
2024-09-28 20:09:12 +0200

    type hints

--- info_text.py
+++ info_text.py
@@ -5,7 +5,7 @@ def dawn() -> str:
     """
     return explanation
 
-def earliest_tallit():
+def earliest_tallit() -> str:
     explanation = """Misheyakir occurs just after dawn. It is traditionally defined as
     the time at which one can recognize another person four cubits away. This is the earliest time to
     wear tzitzit and tefilin and to recite the Shema.
@@ -13,13 +13,13 @@ def earliest_tallit():
     This is calculated to be when the centre point of the sun's disk is 10.2 degrees below the horizon."""
     return explanation
 
-def sunrise():
+def sunrise() -> str:
     explanation = """Sunrise is the moment when the top of sun's disk becomes
     visible at sea level. One should not begin the morning Amidah before sunrise.
     Most daytime mitzvot should be performed after this time."""
     return explanation
 
-def latest_shema():
+def latest_shema() -> str:
     explanation = """The latest time to recite the morning Shema.
     This is the end of the first quater of a halachic day.
     This means three 'proportional' hours or shaos zmaniyos have passed.
@@ -28,52 +28,52 @@ def latest_shema():
     halachic sunrise."""
     return explanation
 
-def latest_shacharit():
+def latest_shacharit() -> str:
     explanation = """The latest time to say the morning Amidah.
     It is the end of the first third of a halachic day. This is
     simply calculated by adding four proportional hours to the halachic
     sunrise."""
     return explanation
 
-def midday():
+def midday() -> str:
     explanation = """Chatzos is the halachic noon and the midpoint of the
     halachic day. This is calculated as the midpoint between the halachic sunrise
     and sunset."""
     return explanation
 
-def earliest_mincha():
+def earliest_mincha() -> str:
     explanation = """This is the earliest time to recite Mincha.
     It is defined as half a halachic hour after midday and therefore calculated
     as 6.5 proportional hours after halachic sunrise.
     """
     return explanation
 
-def mincha_ketanah():
+def mincha_ketanah() -> str:
     explanation = """Mincha ketanah litterally translated as the smaller Mincha
     is considered the ideal time for praying Mincha.
     It is calculated as 2.5 proportional hours before the halachic sunset.
     """
     return explanation
 
-def plag_hamincha():
+def plag_hamincha() -> str:
     explanation = """The midpoint between Mincha ketanah and sunset as well
     as the earliest time one may light Shabbat candles.
     It is calculated as 1.25 proportional hours before the halachic sunset."""
     return explanation
 
-def candle_lighting():
+def candle_lighting() -> str:
     explanation = """Candles should be lit before this time on Erev Shabbat and Yom Tov.
     It is a mitzvah to add a few minutes to Shabbat before it starts and ends.
     As is the most prevalent custom this time is calculated as 18 minutes before sunset"""
     return explanation
 
-def sunset():
+def sunset() -> str:
     explanation = """Sunset is the moment when the top of the sun's disk
     falls below the horizon. A new day in the Hebrew calendar begins at this
     time. All daytime mitzvot should be completed before sunset."""
     return explanation
 
-def nightfall():
+def nightfall() -> str:
     explanation = """Nightfall is generally considered to being when three
     stars become visible in the night sky. This marks the time from which all
     nighttime mitzvot may be performed such as reciting the evening Shema.
@@ -82,14 +82,14 @@ def nightfall():
     """
     return explanation
 
-def midnight():
+def midnight() -> str:
     explanation = """One should try to complete all nightime mitzvot
     before this time such as the nightly Shema and the counting of the Omer.
     Chatzot or halachic midnight is calculated as the midpoint between
     halachic sunrise and halachic sunset."""
     return explanation
 
-def shaah_zmanit():
+def shaah_zmanit() -> str:
     explanation = """A halachic or proportional hour. Shaos zmaniyos are
     each 1/12th of the halachic day."""
     return explanation
